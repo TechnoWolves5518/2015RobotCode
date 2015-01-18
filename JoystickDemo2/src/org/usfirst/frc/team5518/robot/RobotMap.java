bc4124b443e03cffa6479b59d400bf0067abb9e4
hybrezz54 <haz540@gmail.com>
2015-01-18 14:35:41 -0500

    Update 1/18/2015

--- JoystickDemo2/src/org/usfirst/frc/team5518/robot/RobotMap.java
+++ JoystickDemo2/src/org/usfirst/frc/team5518/robot/RobotMap.java
@@ -17,8 +17,19 @@ public class RobotMap {
     // public static int rangefinderModule = 1;
 	
 	public static final int FRONT_LEFT_MOTOR = 2;
-	public static final int REAR_LEFT_MOTOR = 1;
-	public static final int FRONT_RIGHT_MOTOR = 3;
+	public static final int REAR_LEFT_MOTOR = 1; 
+	public static final int FRONT_RIGHT_MOTOR = 3; 
 	public static final int REAR_RIGHT_MOTOR = 0;
+	
+	public static final int JOYSTICK_PORT = 0;
+	public static final int SENSITIVITY_BUFFER = 1;
+	public static final int X_AXIS = 0;
+	public static final int Y_AXIS = 1;
+	public static final int Z_AXIS = 2;   // joystick rotate axis
+	public static final int TRIGGER_AXIS = 4;  // xbox trigger axis
+	public static final int BTN_TRIGGER = 0;   // joystick trigger button & xbox A button
+	
+	public static final int SOLENOID_PCM_PORT1 = 0;
+	public static final int SOLENOID_PCM_PORT2 = 1;
 	
 }
